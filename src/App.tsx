93226d5240953e6bd6bdfe95b15de4c3253bc1bb
Sébastien Candau <sebastiencandau>
2024-01-28 22:27:47 +0100

    replace imports with require

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import MenuChapterOne from './views/Menu/MenuChapterOne';
 import Conversation from './views/Conversation/Conversation';
 import { narativeIndicationsForChapter } from './utils/chapters.utils';
 import NarativeScreen from './components/NarativeScreen';
-import max_chloe_music from './assets/musics/max_and_chloe.mp3';
-import chapter_one_ending_music from './assets/musics/chapter_one_ending_music.mp3'
 
 const App: React.FC = () => {
   const [chapter, setChapter] = useState<number>(localStorage.getItem('chapter') ? JSON.parse(localStorage.getItem('chapter')!) : 1);
@@ -45,13 +43,13 @@ const App: React.FC = () => {
     <div>
       {indicationsState ? (
         <div>
-          <audio autoPlay src={max_chloe_music} id="audio" loop />
+          <audio autoPlay src={require('./assets/musics/max_and_chloe.mp3')} id="audio" loop />
           <NarativeScreen startConversation={startConversation} currentIndications={narativeIndicationsForChapter(chapter)!} />
         </div>
       ) : !chapterStarted && (chapter === undefined || (chapter >= 1 && chapter <= 6) || chapter === 999) ? (
         <div>
-          { (chapter <6 || chapter === 999) ? <audio autoPlay src={max_chloe_music} id="audio" loop /> :
-          <audio autoPlay src={chapter_one_ending_music} id="audio" loop />
+          { (chapter <6 || chapter === 999) ? <audio autoPlay src={require('./assets/musics/max_and_chloe.mp3')} id="audio" loop /> :
+          <audio autoPlay src={require('./assets/musics/chapter_one_ending_music.mp3')} id="audio" loop />
           }
           <MenuChapterOne gameState={gameStarted} chapter={chapter} startGame={startGame} />
         </div>
