3764036f43e531cd11414e404d5e69839049a6b6
sebastiencandau <sebastien.candau@cap-metiers.pro>
2024-05-29 15:30:53 +0200

    end of chapter 1000

--- src/utils/chapterTwo/chapterOneThousand.utils.ts
+++ src/utils/chapterTwo/chapterOneThousand.utils.ts
@@ -491,67 +491,314 @@ export const followingMessage = async (message: string, playerName: string) => {
                     `pourquoi "Les coeurs brisés" ?`
                 ]
             };
-        case "ce doit être si dur à vivre":
+        case "alors tu penses qu'elle s'est vraiment suicidée ?":
+        case "personne ne lui a fait du mal selon toi ?":
             return {
                 messages: [
                     {
-                        msg: "le plus dur",
+                        msg: "c'est le plus probable selon moi",
                         type: null,
                         received: true
                     },
                     {
-                        msg: "c'est de le cacher à ses proches",
-                        type: null,
-                        received: true
-                    },
-                    {
-                        msg: `Lucie était très pudique sur ses souffrances`,
-                        type: null,
-                        received: true
-                    },
-                    {
-                        msg: `et elle ne voulait surtout pas que ce soit un fardeau pour ses proches`,
-                        type: null,
-                        received: true
-                    },
-                    {
-                        msg: `alors elle le cachait.`,
+                        msg: "mais un doute persiste",
                         type: null,
                         received: true
                     }
                 ],
                 choices: [
-                    "alors tu penses qu'elle s'est vraiment suicidée ?",
-                    `pourquoi "Les coeurs brisés" ?`
+                    "un doute ?",
+                    `dis moi tout`
                 ]
             };
-        case "ce doit être si dur à vivre":
+            case "j'aurais aimé être là pour elle":
+                return {
+                    messages: [
+                        {
+                            msg: "on l'a été tout les deux",
+                            type: null,
+                            received: true
+                        },
+                        {
+                            msg: `je pense que parfois les choses nous dépassent ${playerName}`,
+                            type: null,
+                            received: true
+                        }
+                    ],
+                    choices: [
+                        "alors tu penses qu'elle s'est vraiment suicidée ?",
+                        `personne ne lui a fait du mal selon toi ?`
+                    ]
+                };
+        case "dis moi tout":
+        case "un doute ?":
             return {
                 messages: [
                     {
-                        msg: "Je les appelle 'Les coeurs brisés' car pour beaucoup d'entre eux, cette sensibilité semble être un trait de caractère profondément enraciné,",
+                        msg: "Brice",
                         type: null,
                         received: true
                     },
                     {
-                        msg: "presque comme si elle était inscrite dans leur ADN",
+                        msg: "ce gars est pas net",
                         type: null,
                         received: true
                     },
                     {
-                        msg: `C'est comme s'ils étaient nés avec cette capacité à ressentir intensément les émotions`,
-                        type: null,
-                        received: true
-                    },
-                    {
-                        msg: `les rendant plus vulnérables aux blessures de la vie`,
+                        msg: `il traine dans de sales histoires`,
                         type: null,
                         received: true
                     },
                 ],
                 choices: [
-                    "alors tu penses qu'elle s'est vraiment suicidée ?",
-                    `j'aurais aimé être là pour elle`
+                    "quelles histoires ?",
+                    `tu penses qu'il aurait pu lui faire du mal ?`
+                ]
+            };
+        case "quelles histoires ?":
+        case `tu penses qu'il aurait pu lui faire du mal ?`:
+            return {
+                messages: [
+                    {
+                        msg: "je sais pas trop",
+                        type: null,
+                        received: true
+                    },
+                    {
+                        msg: "mais parfois Lucie se confiait à moi sur sa relation",
+                        type: null,
+                        received: true
+                    },
+                    {
+                        msg: `et j'ai pu comprendre qu'il était violent`,
+                        type: null,
+                        received: true
+                    },
+                    {
+                        msg: `elle avait peur de lui`,
+                        type: null,
+                        received: true
+                    },
+                ],
+                choices: [
+                    "peur à quel point ?",
+                    `violent comment ?`
+                ]
+            };
+        case "peur à quel point ?":
+        case "violent comment ?":
+            return {
+                messages: [
+                    {
+                        msg: "au point où elle m'a un jour mise en garde",
+                        type: null,
+                        received: true
+                    },
+                    {
+                        msg: "au point où elle m'a un jour mise en garde",
+                        type: "indication",
+                        received: true
+                    },
+                    {
+                        msg: `Sarah vous transfère un message`,
+                        type: null,
+                        received: true
+                    },
+                    {
+                        msg: `"Sarah si un jour il m'arrive quelque chose, c'est Brice"`,
+                        type: "indication",
+                        received: true
+                    },
+                ],
+                choices: [
+                    "wah...",
+                    `alors c'est lui.`
+                ]
+            };
+        case "wah...":
+        case "alors c'est lui.":
+            return {
+                messages: [
+                    {
+                        msg: "je vais mener l'enquête Sarah",
+                        type: null,
+                        received: false
+                    },
+                    {
+                        msg: "laisse moi t'aider",
+                        type: null,
+                        received: true
+                    }
+                ],
+                choices: [
+                    "(accepter)",
+                    `(refuser)`
+                ]
+            };
+        case "(accepter)":
+            choices = JSON.parse(await localStorage.getItem('choices')!)
+            choices[7] = 'sarah_colaborate';
+            localStorage.setItem('choices', JSON.stringify(choices));
+            return {
+                messages: [
+                    {
+                        msg: "ce choix aura des conséquences",
+                        type: 'indication',
+                        received: true
+                    },
+                    {
+                        msg: "bienvenue dans l'équipe !",
+                        type: null,
+                        received: true
+                    },
+                    {
+                        msg: `bien chef 🫡`,
+                        type: null,
+                        received: true
+                    },
+                    {
+                        msg: `il nous faut des noms de code !`,
+                        type: null,
+                        received: true
+                    },
+                    {
+                        msg: `choisis pour moi`,
+                        type: null,
+                        received: true
+                    },
+                ],
+                choices: [
+                    "Baker",
+                    `Trinity`,
+                    "BranleX2000"
+                ]
+            };
+
+        case "(refuser)":
+            choices = JSON.parse(await localStorage.getItem('choices')!)
+            choices[7] = 'sarah_alone';
+            localStorage.setItem('choices', JSON.stringify(choices));
+            return {
+                messages: [
+                    {
+                        msg: "ce choix aura des conséquences",
+                        type: 'indication',
+                        received: true
+                    },
+                    {
+                        msg: "c'est mieux pour toi si tu restes en dehors de ça Sarah...",
+                        type: null,
+                        received: true
+                    },
+                    {
+                        msg: `je comprends...`,
+                        type: null,
+                        received: true
+                    },
+                    {
+                        msg: `si tu as besoin de quoi que ce soit... tu peux compter sur moi`,
+                        type: null,
+                        received: true
+                    },
+                    {
+                        msg: `FIN DE LA SECONDE PARTIE`,
+                        type: 'indication',
+                        received: true
+                    },
+                ],
+                choices: [
+                ]
+            };
+
+        case "Baker":
+            localStorage.setItem('pseudo', JSON.stringify('agent 007'));
+            return {
+                messages: [
+                    {
+                        msg: "génial",
+                        type: null,
+                        received: true
+                    },
+                    {
+                        msg: `toi tu seras`,
+                        type: null,
+                        received: true
+                    },
+                    {
+                        msg: `agent 007`,
+                        type: null,
+                        received: true
+                    },
+                    {
+                        msg: `FIN DE LA SECONDE PARTIE`,
+                        type: 'indication',
+                        received: true
+                    },
+                ],
+                choices: [
+                ]
+            };
+
+        case "Trinity":
+            localStorage.setItem('pseudo', JSON.stringify('Morpheus'));
+            return {
+                messages: [
+                    {
+                        msg: "classe 🤖",
+                        type: null,
+                        received: true
+                    },
+                    {
+                        msg: `toi tu seras`,
+                        type: null,
+                        received: true
+                    },
+                    {
+                        msg: `Morpheus`,
+                        type: null,
+                        received: true
+                    },
+                    {
+                        msg: `FIN DE LA SECONDE PARTIE`,
+                        type: 'indication',
+                        received: true
+                    },
+                ],
+                choices: [
+                ]
+            };
+
+        case "BranleX2000":
+            localStorage.setItem('pseudo', JSON.stringify('CiCACA3301'));
+            return {
+                messages: [
+                    {
+                        msg: "MDRRR",
+                        type: null,
+                        received: true
+                    },
+                    {
+                        msg: `ça me va`,
+                        type: null,
+                        received: true
+                    },
+                    {
+                        msg: `alors toi tu seras...`,
+                        type: null,
+                        received: true
+                    },
+                    {
+                        msg: `CiCACA3301`,
+                        type: null,
+                        received: true
+                    },
+                    {
+                        msg: `FIN DE LA SECONDE PARTIE`,
+                        type: 'indication',
+                        received: true
+                    },
+                ],
+                choices: [
                 ]
             };
 
